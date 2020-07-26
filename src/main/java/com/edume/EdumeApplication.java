bd426e72f87b5120c220e53532d12d9820afa1da
m.abdelbaset.dev@gmail.com <m.abdelbaset.dev@gmail.com>
2020-07-26 21:47:17 +0200

    init boot project

---
+++ src/main/java/com/edume/EdumeApplication.java
@@ -0,0 +1,12 @@
+package com.edume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdumeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/edume/EdumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
